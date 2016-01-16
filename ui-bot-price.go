fcbac3d093d84aeec5f432bb1b5726eef93f5237
Ilnur Sayfutdinov <ilnursoft@gmail.com>
2016-01-16 14:16:00 +0300

    add func save cfg file using input data from web page

--- ui-bot-price.go
+++ ui-bot-price.go
@@ -4,6 +4,7 @@ import (
 	//	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 	"strconv"
 
 	//	"image"
@@ -35,10 +36,32 @@ type Tasker struct {
 
 var taskT TaskerTovar
 
+var tekuser string // текущий пользователь который задает условия на срабатывания
+
 type page struct {
-	Title string
-	Msg   string
-	Msg2  string
+	Title  string
+	Msg    string
+	Msg2   string
+	TekUsr string
+}
+
+func Savestrtofile(namef string, str string) int {
+	file, err := os.Create(namef)
+	if err != nil {
+		// handle the error here
+		return -1
+	}
+	defer file.Close()
+
+	file.WriteString(str)
+	return 0
+}
+
+//http://www.labirint.ru/books/408438/;<;1534;
+//сохранение данных из Tasker товара в файл с именем namef
+func savetofilecfg(namef string, t TaskerTovar) {
+	str := t.Url + ";" + t.uslovie + ";" + strconv.Itoa(t.Tasker.price) + ";" + "\n"
+	Savestrtofile(namef, str)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -48,12 +71,15 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 	if title != "exec/" {
 		t, _ := template.ParseFiles("template.html")
-		t.Execute(w, &page{Title: "Создание триггера", Msg: "Задание триггера (условия) на срабатывание бота цен"})
+		t.Execute(w, &page{Title: "Создание триггера", Msg: "Задание триггера (условия) на срабатывание бота цен", TekUsr: "Текущий пользователь: " + tekuser})
 	} else {
 		shop := r.FormValue("shop")
 		taskT.Url = r.FormValue("surl")
 		taskT.uslovie = r.FormValue("uslovie")
 		taskT.Tasker.price, _ = strconv.Atoi(r.FormValue("schislo"))
+
+		savetofilecfg(shop+"-url.cfg", taskT)
+
 		ss1 := "Введенное условие для магазина " + shop
 		ss := taskT.Url + "   " + taskT.uslovie + " " + r.FormValue("schislo")
 		t1, _ := template.ParseFiles("template-result.html")
@@ -63,6 +89,8 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	tekuser = "testuser"
+
 	http.HandleFunc("/", index)
 
 	http.ListenAndServe(":7777", nil)
